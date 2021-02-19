c56aea050d688380d7ba920cab4037cac8cc6fd8
venusdharan <venusdharan@gmail.com>
2021-02-19 20:41:07 +0530

    spa

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,11 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+ target: 'static',
 
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  mode: 'spa',
+
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
